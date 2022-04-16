0b3d746681313148efe011bd11658815e29503cb
Fatih Tuzun <fatihtuzun11@gmail.com>
2022-04-17 00:48:41 +0300

    routes done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 
+//routes
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products';
+
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+    <h1>Home</h1>
+    <h2>hello world</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipis
+    icing elit. Nobis laboriosam vero quae consequatur 
+    temporibus dignissimos cumque tempore autem, ab, c
+    ulpa voluptates nesciunt tempora voluptatum quasi, 
+    neque reprehenderit voluptate impedit at.</p>
+    </div>
+  )
+}
+
+export default Home
